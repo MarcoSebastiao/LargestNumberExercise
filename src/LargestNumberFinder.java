ae6b52a0e2b41a5ab4f169346995a74c6bac8dba
MarcoSebastiao <marco.j.sebastiao@gmail.com>
2022-07-27 20:57:20 +0100

    +getLargestNumberAlt faster output generation

--- src/LargestNumberFinder.java
+++ src/LargestNumberFinder.java
@@ -3,11 +3,25 @@ import java.util.stream.Collectors;
 
 public class LargestNumberFinder {
     public String getLargestNumber(List<Integer> numberPool) {
-        numberPool.sort((o1, o2) -> {
+        sortIntList(numberPool);
+        return numberPool.stream().map(Object::toString).collect(Collectors.joining());
+    }
+
+    /*
+    * Faster
+    * */
+    public String getLargestNumberAlt(List<Integer> numberPool) {
+        sortIntList(numberPool);
+        StringBuilder sb = new StringBuilder();
+        numberPool.forEach(sb::append);
+        return sb.toString();
+    }
+
+    private void sortIntList(List<Integer> list) {
+        list.sort((o1, o2) -> {
             String v1 = "" + o1 + o2;
             String v2 = "" + o2 + o1;
             return v2.compareTo(v1);
         });
-        return numberPool.stream().map(Object::toString).collect(Collectors.joining());
     }
 }
---
+++ src/LargestNumberFinderTest.java
@@ -0,0 +1,39 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.RepeatedTest;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+public class LargestNumberFinderTest {
+
+    private static LargestNumberFinder ln;
+    private static HashMap<String, List> dataSet;
+    private static List<String> keys;
+
+    @BeforeAll
+    public static void init() {
+        ln = new LargestNumberFinder();
+        dataSet = new HashMap<>();
+        dataSet.put("95021", new ArrayList<>(List.of(9, 50, 1, 2)));
+        dataSet.put("56550", new ArrayList<>(List.of(56, 5, 50)));
+        dataSet.put("42423420", new ArrayList<>(List.of(42, 423, 420)));
+        dataSet.put("97585", new ArrayList<>(List.of(9, 5, 58, 7)));
+        keys = new ArrayList<>(dataSet.keySet());
+    }
+
+    @RepeatedTest(15000)
+    public void getLargestNumber() {
+        for (String key : keys) {
+            Assertions.assertEquals(key, ln.getLargestNumber(dataSet.get(key)));
+        }
+    }
+
+    @RepeatedTest(15000)
+    public void getLargestNumberAlt() {
+        for (String key : keys) {
+            Assertions.assertEquals(key, ln.getLargestNumberAlt(dataSet.get(key)));
+        }
+    }
+}
