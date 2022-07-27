63d117abca18b2766be039567679ec2bb6d35eee
MarcoSebastiao <marco.j.sebastiao@gmail.com>
2022-07-27 20:28:58 +0100

    Working code v1

--- src/LargestNumberFinder.java
+++ src/LargestNumberFinder.java
@@ -1,7 +1,13 @@
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class LargestNumberFinder {
     public String getLargestNumber(List<Integer> numberPool) {
-        return "";
+        numberPool.sort((o1, o2) -> {
+            String v1 = "" + o1 + o2;
+            String v2 = "" + o2 + o1;
+            return v2.compareTo(v1);
+        });
+        return numberPool.stream().map(Object::toString).collect(Collectors.joining());
     }
 }
--- src/LargestNumberTest.java
+++ src/LargestNumberTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LargestNumberTest {
@@ -8,8 +9,8 @@ public class LargestNumberTest {
     @Test
     public void getLargestNumber() {
         LargestNumberFinder ln = new LargestNumberFinder();
-        Assertions.assertEquals("95021", ln.getLargestNumber(List.of(9, 50, 1, 2)));
-        Assertions.assertEquals("56550", ln.getLargestNumber(List.of(56, 5, 50)));
-        Assertions.assertEquals("42423420", ln.getLargestNumber(List.of(42, 423, 420)));
+        Assertions.assertEquals("95021", ln.getLargestNumber(new ArrayList<>(List.of(9, 50, 1, 2))));
+        Assertions.assertEquals("56550", ln.getLargestNumber(new ArrayList<>(List.of(56, 5, 50))));
+        Assertions.assertEquals("42423420", ln.getLargestNumber(new ArrayList<>(List.of(42, 423, 420))));
     }
 }
